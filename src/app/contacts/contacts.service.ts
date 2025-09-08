0c13b90adba2721b0903f62d4c4af1b154cbfe59
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-08 12:58:15 +0200

    feat: add + view

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Contact } from '../models/contact';
 import { CONTACTS } from '../data/contacts';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class ContactsService {
     this.contacts.push(contact);
   }
 
-  
+  public GetContactById(id: number): Observable<Contact | undefined> {
+    const contact = this.contacts.find(contact => contact.id === id);
+    return of(contact);
+  }
 }
--- src/app/contacts/view/view.component.ts
+++ src/app/contacts/view/view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../models/contact';
+import { ActivatedRoute, } from '@angular/router';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-view',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
-export class ViewComponent {
+export class ViewComponent implements OnInit {
+  contact: Contact | null = null;
+  contactId: number | null = null;
 
+  constructor( 
+    private route: ActivatedRoute,
+    private contactService: ContactsService
+  ) {}
+
+  ngOnInit(): void {
+    this.contactId = Number(this.route.snapshot.paramMap.get('id'));
+    this.contactService.GetContactById(this.contactId).subscribe(contact => {
+      this.contact = contact!;
+    });
+  }
 }
