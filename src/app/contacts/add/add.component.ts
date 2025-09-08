d83587e917deb5b103e45350031a89c02888d363
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-08 11:47:58 +0200

    feat: contacts list and menu

---
+++ src/app/contacts/add/add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  standalone: false,
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent {
+
+}
---
+++ src/app/contacts/contacts.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../models/contact';
+import { CONTACTS } from '../data/contacts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  public contacts: Contact[] = CONTACTS;
+
+  public AddContact(contact: Contact): void {
+    this.contacts.push(contact);
+  }
+
+  
+}
